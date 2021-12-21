86211d53b44f66872da8208618f6b48a588ff701
JaelNgutu <Jaelngutu@gmail.com>
2021-12-21 19:28:39 +0300

    Implementing different types of injection

--- src/main/java/com/example/dependency/controllers/ConstructorInjectedController.java
+++ src/main/java/com/example/dependency/controllers/ConstructorInjectedController.java
@@ -1,10 +1,12 @@
 package com.example.dependency.controllers;
 
 import com.example.dependency.services.GreetingService;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ConstructorInjectedController {
     private final GreetingService greetingService;
-
+//Autowired annotation optional on constrictors
     public ConstructorInjectedController(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
